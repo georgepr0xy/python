9277c24e0b9219a1b9c09978b3733e30d816ed93
georgepr0xy <99556878+georgepr0xy@users.noreply.github.com>
2022-04-25 14:58:13 +0530

    Create distance between two points.py

---
+++ distance between two points.py
@@ -0,0 +1,6 @@
+a1 = float(input('point a1='))
+a2 = float(input('point a2='))
+b1 = float(input('point b1='))
+b2 = float(input('point b2='))
+Distance=((b2-b1)**2 + (a2-a1)**2)**0.5
+print('The distance between the point is = %f' %Distance)
